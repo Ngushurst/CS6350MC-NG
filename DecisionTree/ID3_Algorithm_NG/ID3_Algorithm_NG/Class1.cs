476e063697108875ab462ddc11ee38f028001a2b
Noah <Ngushurst@gmail.com>
2020-02-06 14:18:05 -0700

    Starting Assignment 1
    I've made a new project in C# and got the examples downloaded (and am uploading them for future tests). Cheers.

---
+++ DecisionTree/ID3_Algorithm_NG/ID3_Algorithm_NG/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ID3_Algorithm_NG
+{
+    public class Class1
+    {
+    }
+}
