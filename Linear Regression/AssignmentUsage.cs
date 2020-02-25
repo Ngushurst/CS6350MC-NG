910be0844deb0480389b92ddd765c8fcc4f4a626
Noah Gushurst <ngushurst@gmail.com>
2020-02-25 02:57:24 -0700

    Wrote up the Gradient Decent Algorithms
    They were quick to make, debug, and run.
    
    And now it's just to the written bits. If I'd given myself more than three hours, the code might be anything but incredibly lazy. However, we can't all get what we want, so I'll just have to smooth this stuff out later.
    
    Cheers.

---
+++ Linear Regression/AssignmentUsage.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linear_Regression
+{
+    class AssignmentUsage
+    {
+        public static string TestPath = @"..\..\TestingData";
+        public static double limit = Math.Pow(10, -6); //when the output of a gradient decent operation is less than Limit, stop the program and write to file
+
+
+        public static void Main()
+        {
+            List<Reach> ConcreteTrain = readConcreteCSV(TestPath + @"/Concrete/slump_train.csv"); //53 items
+            List<Reach> ConcreteTest = readConcreteCSV(TestPath + @"/Concrete/slump_test.csv"); //50 items
+
+            //Batch Gradient Decent
+            if (true)
+            {
+                GradientDecent Batch = new GradientDecent(ConcreteTrain);
+                StringBuilder output = new StringBuilder();
+                output.Append("Num, Weight,-,-,-,-,-,-,LearnRate,TrainError\n");
+
+                int itCount = 0;
+                double limitTest = double.PositiveInfinity;
+
+                while (limitTest > limit)
+                {
+                    output.Append(itCount + ",");
+                    itCount++; //increment
+                    limitTest = Batch.Batch(); //update weight and get value to test against limit
+
+                    double[] CurrentWeight = Batch.getWeight();
+
+                    for(int i = 0; i < 7; i++) //write current weight to stringbuilder
+                    {
+                        output.Append(CurrentWeight[i] + ",");
+                    }
+                    //slap on learning rate and error. Go do it again
+                    output.Append(Batch.getLearningRate() + "," + Batch.getError() + "\n");
+
+                    //report to user
+                    Console.WriteLine("Finished one iteration of Batch Gradient Decent with a training error of " + Batch.getError());
+                }
+
+                //found a convergent weight vector
+
+                output.Append("Final\n,");
+
+                double[] FinalWeight = Batch.getWeight();
+                for (int i = 0; i < 7; i++) //write current weight to stringbuilder
+                {
+                    output.Append(FinalWeight[i] + ",");
+                }
+
+
+                output.Append(Batch.getLearningRate() + "," + Batch.CalculateAverageError(ConcreteTest)); //slap on test error
+
+                Console.WriteLine("Writing all results to Linear Regression/TestingData/RunResults/ResultsBatch.csv\n\n\n");
+                System.IO.File.WriteAllText(TestPath + @"/RunResults/ResultBatch.csv", output.ToString());
+
+            }
+            //Stochastic Gradient Decent
+            if (true)
+            {
+                GradientDecent Stochastic = new GradientDecent(ConcreteTrain);
+                StringBuilder output = new StringBuilder();
+                output.Append("Num, Weight,-,-,-,-,-,-,LearnRate,TrainError\n");
+
+                int itCount = 0;
+                double limitTest = double.PositiveInfinity;
+
+                while (limitTest > limit)
+                {
+                    output.Append(itCount + ",");
+                    itCount++; //increment
+                    limitTest = Stochastic.Stochastic(); //update weight and get value to test against limit
+
+                    double[] CurrentWeight = Stochastic.getWeight();
+
+                    for (int i = 0; i < 7; i++) //write current weight to stringbuilder
+                    {
+                        output.Append(CurrentWeight[i] + ",");
+                    }
+                    //slap on learning rate and error. Go do it again
+                    output.Append(Stochastic.getLearningRate() + "," + Stochastic.getError() + "\n");
+
+                    //report to user
+                    Console.WriteLine("Finished one iteration of Stochastic Gradient Decent with a training error of " + Stochastic.getError());
+                }
+
+                //found a convergent weight vector
+
+                output.Append("Final\n,");
+
+                double[] FinalWeight = Stochastic.getWeight();
+                for (int i = 0; i < 7; i++) //write current weight to stringbuilder
+                {
+                    output.Append(FinalWeight[i] + ",");
+                }
+
+
+                output.Append(Stochastic.getLearningRate() + "," + Stochastic.CalculateAverageError(ConcreteTest)); //slap on test error
+
+                Console.WriteLine("Writing all results to Linear Regression/TestingData/RunResults/ResultsStochastic.csv");
+                System.IO.File.WriteAllText(TestPath + @"/RunResults/ResultStochastic.csv", output.ToString());
+            }
+        }
+
+
+
+        /// <summary>
+        /// 7 predictors starting at the second item, and then the goal value. A quick and drity function for reading the concrete examples and testing for SLUMP values.
+        /// </summary>
+        private static List<Reach> readConcreteCSV(String Filepath)
+        {
+            //read all lines of the CSV file
+            String[] input = System.IO.File.ReadAllLines(Filepath);
+            if (input.Length == 0)
+            {
+                throw new MissingFieldException("File must have at least one data point");
+            }
+
+            String[][] rawCases = new String[input.Length][];
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                rawCases[i] = input[i].Split(','); //split all the strings and store them in raw cases
+            }
+
+            List<Reach> output = new List<Reach>();
+
+            for(int i = 0; i < rawCases.Length; i++)
+            {
+                double[] attVals = new double[7];
+
+                for(int j = 0; j < 7; j++)
+                {
+                    attVals[j] = Double.Parse(rawCases[i][j + 1]);
+                }
+
+                output.Add(new Reach(attVals, Double.Parse(rawCases[i][8])));
+            }
+
+            return output;
+        }
+
+    }
+}
---
+++ Linear Regression/GradientDecent.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Linear_Regression
+{
+
+    /// <summary>
+    /// A class used to iteratively report weight vectors based on an input data set. Data must be composed fully of numbers, and not made 
+    /// of typical cases (by this library's standards).
+    /// </summary>
+    public class GradientDecent
+    {
+        double[] Weight; //That's all a gradient decent learner really is.
+        double LearningRate = 1;
+        double PreviousError = double.PositiveInfinity;
+        int StochasticIndex = 0; //index of the next item to use from the data for stochastic gradient decent
+        List<Reach> TrainingData;
+
+
+        /// <summary>
+        /// Gradient decent learner initialized the given weight vector
+        /// </summary>
+        public GradientDecent(double[] weight, List<Reach> Train)
+        {
+            Weight = weight.ToArray();
+            TrainingData = Train.ToList();
+        }
+
+
+        /// <summary>
+        /// Gradient decent learner initialized to contain the zero vector with dimentions equal to the data's attributes (don't include the target attribute in your count)
+        /// Returns the norm (length) of the difference of the new weight vector minus the previous length vector.
+        /// </summary>
+        public GradientDecent(List<Reach> Train)
+        {
+            Weight = new double[Train[0].attributeVals.Length];
+            for (int i = 0; i < Train[0].attributeVals.Length; i++) //initialize all doubles to zero
+            {
+                Weight[i] = 0.0;
+            }
+
+            TrainingData = Train.ToList();
+        }
+
+
+        /// <summary>
+        /// Updates the weight vector
+        /// </summary>
+        public double Batch()
+        {
+            double[] newWeight = GD(Weight, TrainingData, LearningRate);
+
+            double output = WeightVectorDifference(newWeight, Weight);
+
+            Weight = newWeight;
+
+            upDateLR();
+
+            return output;
+        }
+
+        /// <summary>
+        /// Same as batch gradient decent but uses one item from the data for each calculation.
+        /// </summary>
+        public double Stochastic()
+        {
+            List<Reach> nextItem = new List<Reach>(1);
+            nextItem.Add(TrainingData[StochasticIndex]); //put in the next item
+
+            double[] newWeight = GD(Weight, nextItem, LearningRate);
+
+            double output = WeightVectorDifference(newWeight, Weight);
+
+            Weight = newWeight;
+
+            upDateLR();
+
+            return output;
+        }
+
+        /// <summary>
+        /// Returns ||W_1 - W_2||, or the length of the difference of the two vectors. Smaller values mean a smaller change in weight.
+        /// </summary>
+        private static double WeightVectorDifference(double[] W_1, double[]W_2)
+        {
+            double[] temp = new double[W_1.Length];
+
+            for(int i = 0; i < W_1.Length; i++) //take the difference of the vectors
+            {
+                temp[i] = W_1[i] - W_2[i];
+            }
+
+            //use pythagrian theorem
+            double squareSum = 0;
+            for (int i = 0; i < temp.Length; i++) //take the difference of the vectors
+            {
+                squareSum += Math.Pow(temp[i],2); //square each item and toss it on
+            }
+
+            return Math.Sqrt(squareSum); //finish by taking the square root
+
+        }
+
+        /// <summary>
+        /// Gradient Decent function.
+        /// </summary>
+        private static double[] GD(double[] weight, List<Reach> data, double LearningRate)
+        {
+            double[] newWeight = new double[weight.Length];
+
+            for(int i = 0; i <weight.Length; i++) //initialize all doubles to zero
+            {
+                newWeight[i] = 0.0;
+            }
+
+            foreach(Reach item in data)
+            {
+                double error = CalculateError(item, weight);
+
+                for(int i = 0; i < weight.Length; i++)
+                {
+                    newWeight[i] += error * item.attributeVals[i]; //sum of error times value of each piece of data
+                }
+            }
+
+            for(int i = 0; i < weight.Length; i++)
+            { //negate sum, multiply by learning rate and original wieght, and then subtract from original weight to get new weight.
+                newWeight[i] = weight[i] - LearningRate * -newWeight[i];
+            }
+
+            return newWeight;
+        }
+
+
+        private static double CalculateError(Reach data, double[] weight)
+        {
+            double prediction = 0;
+
+            for(int i = 0; i < data.attributeVals.Length; i++)
+            {
+                prediction += data.attributeVals[i] * weight[i]; //add data times its weight
+            }
+
+            return data.answer - prediction;
+        }
+
+
+        /// <summary>
+        /// Updates the learning rate and training error
+        /// </summary>
+        private void upDateLR()
+        {
+            double error = CalculateAverageError(TrainingData); //get new training error
+            if (!(error < PreviousError)) //passed mid-point. Cut learning rate in half.
+            {
+                LearningRate = LearningRate/2;
+            }
+
+            PreviousError = error;
+        }
+
+        /// <summary>
+        /// Error is also known as cost. Calculates the average error per data point in a given list of data.
+        /// </summary>
+        public double CalculateAverageError(List<Reach> data)
+        {
+            double totalError = 0;
+
+            foreach(Reach item in data)
+            {
+                totalError += CalculateError(item, Weight);
+            }
+
+            return totalError / data.Count;
+        }
+
+        public double getLearningRate()
+        {
+            return LearningRate;
+        }
+
+        public double[] getWeight()
+        {
+            double[] output = Weight.ToArray();
+            return output;
+        }
+
+        public double getError()
+        {
+            return PreviousError;
+        }
+    }
+
+    public class Reach
+    {
+        public readonly double[] attributeVals;
+
+        public readonly double answer;
+
+        public Reach(double[] attributes, double expectedValue)
+        {
+            attributeVals = attributes.ToArray(); //copy rather than reference
+            answer = expectedValue;
+        }
+
+    }
+}
